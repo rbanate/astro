31ad6477811d933aac222afb74104248b20dfc42
Ricardo Banate <ricardo.banate@gmail.com>
2023-05-15 13:56:56 +0800

    init files

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,4 @@
+-- TODO: test
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +10,40 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        -- event = "User Astrfile",
+      }
+    end,
+    event = "User Astrofile",
+    cmd = { "TodoQuickFix" },
+    keys = { "<leader>T", "<cmd>Telescope<cr>", desc = "Open TODOs in Telescope" },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
+  {
+    "Wansmer/treesj",
+    keys = { "<space>m", "<space>j", "<space>s" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup { --[[ your config ]]
+      }
+    end,
+  },
 }
